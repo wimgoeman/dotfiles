0ec99bfcb3afad28c23b72601420f4e9d32b2d9b
Wim Goeman <wim.goeman@amadeus.com>
2023-07-06 11:27:54 +0200

    starship, flameshot & i3 repo

--- private_dot_local/private_share/executable_installall.sh
+++ private_dot_local/private_share/executable_installall.sh
@@ -32,6 +32,22 @@ else
     chmod a+r /etc/apt/sources.list.d/docker.list
 fi
 
+
+# Add i3wm repo
+if [ -f /etc/apt/sources.list.d/sur5r-i3.list ]
+then
+    echo -e "\e[32mi3wm apt sources already configured\e[0m"
+else
+    echo -e "\e[35mConfiguring i3wm apt repos\e[0m"
+    pushd /tmp
+    /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
+    apt install ./keyring.deb
+    popd
+    echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | tee /etc/apt/sources.list.d/sur5r-i3.list
+    chmod a+r /etc/apt/sources.list.d/sur5r-i3.list
+fi
+
+
 # Install all custom packages
 apt update
 apt install \
@@ -59,7 +75,8 @@ apt install \
     golang \
     xdotool \
     ripgrep \
-    flameshot
+    flameshot \
+    nitrogen
 
 if [ -z "$GOPROXY" ]
 then
@@ -70,11 +87,21 @@ PIP_CONF_PATH=/home/$SUDO_USER/.config/pip/pip.conf
 if [ ! -f "$PIP_CONF_PATH" ]
 then
     echo -e "\e[33mPyhton/pip is installed, but not configured. Create a file at $PIP_CONF_PATH to setup a custom repository!\e[0m"
+else
+    echo -e "\e[32mPyhton/pip is already configured\e[0m"
 fi
 
 sudo -u "$SUDO_USER" pip install rofimoji
 
 echo -e "\e[35mSwitching shell to fish\e[0m" 
 chsh "$SUDO_USER" -s /usr/bin/fish
+
+if [ ! -f /usr/local/bin/starship ]
+then 
+    echo -e "\e[35mInstalling starship prompt\e[0m" 
+    curl -sS https://starship.rs/install.sh | sh
+else
+    echo -e "\e[32mStarship prompt already installed\e[0m" 
+fi
 
 echo -e "\e[32mDone!\e[0m" 
